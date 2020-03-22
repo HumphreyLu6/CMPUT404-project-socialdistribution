4ec1cf743de56255398a26ab7d9e13e3fa4f45e2
Yuan Wang <wang17@ualberta.ca>
2020-03-21 20:33:41 -0600

    modify api constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,12 +7,43 @@ if (window.location.port) {
     host = host.replace("3000", "8000")
 }
 const HOST = host.concat("/");
-export const LOGIN_API = HOST + "api/user/login/";
-export const REGISTER_API = HOST + "api/user/signup/";
-export const POST_API = HOST + "api/post/";
-export const AUTHOR_API = HOST + "api/user/author/";
-export const CURRENT_USER_API = HOST + "api/user/author/current_user/";
-export const FRIEND_API = HOST + "api/friend/my_friends/";
-export const FRIEND_REQUEST_API = HOST + "api/friend/friend_request/";
-export const FRIEND_BOOL = HOST + 'api/friend/if_friend/';
-export const USERNAME_LIST = HOST + 'api/user/author/username_list/';
+// login and register api
+export const LOGIN_API = HOST + "login/";
+export const REGISTER_API = HOST + "signup/";
+
+// user api
+export const CURRENT_USER_API = HOST + "author/current_user/";
+export const AUTHOR_LIST_API = HOST + "author";
+export const AUTHOR_PROFILE_API = function (authorId) {
+    return `${HOST}author/${authorId}`;
+}
+export const AUTHOR_GITHUB_API = function (authorId) {
+    return `${HOST}author/${authorId}/github_token`;
+}
+
+// friend api
+export const AUTHOR_FRIENDREQUEST_API = function (authorId){
+    return `${HOST}author/${authorId}/friendrequests`;
+}
+export const FRIENDS_API = function (authorId){
+    return `${HOST}author/${authorId}/friends/`;
+}
+export const FRIEND_REQUEST_API = HOST + 'friendrequest/';
+export const IF_FRIEND_API = function (authorId,friendId){
+    return `${HOST}author/${authorId}/friends/${friendId}`;
+}
+
+// post api
+export const AUTHOR_POST_API = function (authorId){
+    return `${HOST}author/${authorId}/posts`;
+}
+export const VISIBLE_POST_API = HOST + 'author/posts';
+export const POST_API = HOST + 'posts';
+export const SINGLE_POST_API = function (postId) {
+    return `${HOST}posts/${postId}`;
+} 
+
+// comment api
+export const COMMENT_API = function (postId){
+    return `${HOST}posts/${postId}/comments`;
+}
