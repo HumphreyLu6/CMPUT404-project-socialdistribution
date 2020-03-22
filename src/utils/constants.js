d1fe7776ab57d2b9209e2e23ee7e38c490e08b66
Yuan Wang <wang17@ualberta.ca>
2020-03-21 21:01:49 -0600

    adapt api to allow other hosts

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -13,7 +13,9 @@ export const REGISTER_API = HOST + "signup/";
 
 // user api
 export const CURRENT_USER_API = HOST + "author/current_user/";
-export const AUTHOR_LIST_API = HOST + "author";
+export const AUTHOR_LIST_API = function (host){
+    return `${host}author`;
+}
 export const AUTHOR_PROFILE_API = function (authorId) {
     return `${HOST}author/${authorId}`;
 }
@@ -25,25 +27,35 @@ export const AUTHOR_GITHUB_API = function (authorId) {
 export const AUTHOR_FRIENDREQUEST_API = function (authorId){
     return `${HOST}author/${authorId}/friendrequests`;
 }
-export const FRIENDS_API = function (authorId){
-    return `${HOST}author/${authorId}/friends/`;
+export const FRIENDS_API = function (host, authorId){
+    return `${host}author/${authorId}/friends/`;
 }
-export const FRIEND_REQUEST_API = HOST + 'friendrequest/';
-export const IF_FRIEND_API = function (authorId,friendId){
-    return `${HOST}author/${authorId}/friends/${friendId}`;
+export const ANYONE_FRIEND_API = function (host, authorId){
+    return `${host}author/${authorId}/friends`;
+}
+export const FRIEND_REQUEST_API = function (host){
+    return `${host}friendrequest`;
+}
+
+export const IF_AUTHOR_FRIEND_API = function (host, authorId,friendId){
+    return `${host}author/${authorId}/friends/${friendId}`;
 }
 
 // post api
-export const AUTHOR_POST_API = function (authorId){
-    return `${HOST}author/${authorId}/posts`;
+export const AUTHOR_POST_API = function (host, authorId){
+    return `${host}author/${authorId}/posts`;
 }
-export const VISIBLE_POST_API = HOST + 'author/posts';
-export const POST_API = HOST + 'posts';
-export const SINGLE_POST_API = function (postId) {
-    return `${HOST}posts/${postId}`;
+export const VISIBLE_POST_API = function (host){
+    return `${host}author/posts`;
+}
+export const POST_API = function(host){
+    return `${host}posts`;
+}
+export const SINGLE_POST_API = function (host, postId) {
+    return `${host}posts/${postId}`;
 } 
 
 // comment api
-export const COMMENT_API = function (postId){
-    return `${HOST}posts/${postId}/comments`;
+export const COMMENT_API = function (host, postId){
+    return `${host}posts/${postId}/comments`;
 }
