96d85151c9c0cb820dbd9b2d543a6691f38d6c58
Yuan Wang <wang17@ualberta.ca>
2020-03-22 19:04:36 -0600

    combine local posts and remote posts

--- post/views.py
+++ post/views.py
@@ -1,6 +1,7 @@
 import uuid
 import json
 from typing import Tuple, List
+import requests
 from django.db.models import Q
 from django.urls import resolve
 from rest_framework import viewsets, status
@@ -13,7 +14,7 @@ from rest_framework.permissions import (
     IsAuthenticatedOrReadOnly,
 )
 from rest_framework.decorators import action
-
+from node.models import Node
 from user.models import User
 from friend.models import Friend
 from comment.models import Comment
@@ -26,12 +27,14 @@ from .permissions import OwnerOrAdminPermissions
 class PostPagination(PageNumberPagination):
     page_size = 50
     page_size_query_param = "size"
+    
 
     def get_paginated_response(self, data):
+        print(self.get_page_size(self.request))
         return Response(
             {
                 "query": "posts",
-                "count": self.page.paginator.count,
+                "count": len(data) % self.page_size,
                 "size": self.get_page_size(self.request),
                 "next": self.get_next_link(),
                 "previous": self.get_previous_link(),
@@ -93,6 +96,19 @@ class PostsViewSet(viewsets.ModelViewSet):
         else:
             self.permission_classes = [AllowAny]
         return super(PostsViewSet, self).get_permissions()
+
+    def list(self, request, *args, **kwargs):
+        hosts = Node.objects.all().values_list('host',flat=True)
+        for host in hosts:
+            response = requests.get(f"{host}posts/")
+            shared_posts = response.json()
+        visible_posts = Post.objects.filter(visibility='PUBLIC')
+        paged_posts = self.paginate_queryset(visible_posts.order_by("-published"))
+        serializer = PostSerializer(paged_posts, many=True)
+        posts = json.dumps(serializer.data)
+        posts = json.loads(posts)
+        posts += shared_posts['posts']
+        return self.get_paginated_response(posts)
 
     @action(detail=False, methods="GET")
     def visible_posts(self, request, *args, **kwargs):
