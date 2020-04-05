d6fcf713bb8a3122bb0c3cc0ee3608b6d36c7737
Devin <devin@DESKTOP-OL5E28E.localdomain>
2020-04-05 03:25:22 -0600

    finished web UI

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,11 @@
-import { Form, Icon, Input, Button, Checkbox, message } from 'antd';
+import { Form, Icon, Input, Button, Checkbox, message, Layout, Card} from 'antd';
 import React from "react";
 import "antd/dist/antd.css";
 import cookie from 'react-cookies'
 import "./components/Login.css"
+import searocklogin from './Images/searock.jpg'
+import Spongebob from './Images/Spongebob-Squarepants.png'
+import seaweed from './Images/seaplant.png'
 import axios from 'axios';
 import { BE_LOGIN_API_URL, FE_USER_URL,FE_REGISTER_URL } from "./utils/constants.js";
 
@@ -46,47 +49,58 @@ class NormalLoginForm extends React.Component {
     if (this.checkCookie() === true) return;
     const { getFieldDecorator } = this.props.form;
     return (
-      <div>
-        <Form className="login-form">
-          <Form.Item>
-            {getFieldDecorator('Email', {
-              rules: [
-                { required: true, message: 'Please input your address!' },
-                {
-                  type: "email",
-                  message: "The input is not valid E-mail!"
-                }
-              ]
-            })(
-              <Input
-                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                placeholder="Email Address"
-              />,
-            )}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator('password', {
-              rules: [{ required: true, message: 'Please input your Password!' }],
-            })(
-              <Input
-                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                type="password"
-                placeholder="Password"
-              />,
-            )}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator('remember', {
-              valuePropName: 'checked',
-              initialValue: true,
-            })(<Checkbox>Remember me</Checkbox>)}
-          </Form.Item>
-        </Form>
-        <a className="login-to-register" href={FE_REGISTER_URL}>Register</a>
-        <Button type="primary" htmlType="button" className="login-form-button" onClick={this.handleSubmit}>
-          Log in
-        </Button>
-      </div>
+      <Layout>
+        <Layout.Content className='mainpage'>
+            <Card className="login-card">
+              <Form className="login-form">
+                <Form.Item>
+                  {getFieldDecorator('Email', {
+                    rules: [
+                      { required: true, message: 'Please input your address!' },
+                      {
+                        type: "email",
+                        message: "The input is not valid E-mail!"
+                      }
+                    ]
+                  })(
+                    <Input
+                      prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                      placeholder="Email Address"
+                    />,
+                  )}
+                </Form.Item>
+                <Form.Item>
+                  {getFieldDecorator('password', {
+                    rules: [{ required: true, message: 'Please input your Password!' }],
+                  })(
+                    <Input
+                      prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                      type="password"
+                      placeholder="Password"
+                    />,
+                  )}
+                </Form.Item>
+                <Form.Item>
+                  {getFieldDecorator('remember', {
+                    valuePropName: 'checked',
+                    initialValue: true,
+                  })(<Checkbox>Remember me</Checkbox>)}
+                </Form.Item>
+                <Form.Item>
+                  <Button type="primary" htmlType="button" className="login-form-button" onClick={this.handleSubmit}>
+                    Log in
+                  </Button>
+                  <a className="login-to-register" href={FE_REGISTER_URL}>Register</a>
+                </Form.Item>
+              </Form>
+            </Card>
+            <img className="spongebob" src={Spongebob} alt=""></img>
+            <img className="seaweed1" src={seaweed} alt=""></img>
+            <img className="searock" src={searocklogin} alt=""></img>
+
+        </Layout.Content>
+      </Layout>
+      
     );
   }
 }
