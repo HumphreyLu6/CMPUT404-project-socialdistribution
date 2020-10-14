95d1e5c57869e743bd3c82e777ba64882d7d25b9
HumphreyLu6 <zlu@ualberta.ca>
2020-10-13 23:35:14 -0600

    add postgresql server

--- mysite/settings.py
+++ mysite/settings.py
@@ -109,10 +109,21 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "SpongeBookDB",
+        "USER": "SpongeBookMaster",
+        "PASSWORD": "passqwer",
+        "HOST": "spongebookdbinstance.cncaselvmgfu.ca-central-1.rds.amazonaws.com",
+        "PORT": "5432",
     }
 }
 
