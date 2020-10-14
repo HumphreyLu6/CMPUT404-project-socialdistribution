d2509889758aa2933a7035dc4f9fe7359c093630
HumphreyLu6 <zlu@ualberta.ca>
2020-10-14 15:14:27 -0600

    not ignore build

---
+++ build/precache-manifest.ccdd0c59ae7a601ef0c68d04a21427c0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eac5eacb432342945e8c1bc4eaa7ced6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "caa0699a9104b5af1f02",
+    "url": "/static/css/2.9edc93b8.chunk.css"
+  },
+  {
+    "revision": "e53b5017e57eb592cfd0",
+    "url": "/static/css/main.08a8fff3.chunk.css"
+  },
+  {
+    "revision": "caa0699a9104b5af1f02",
+    "url": "/static/js/2.ce019181.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/static/js/2.ce019181.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e53b5017e57eb592cfd0",
+    "url": "/static/js/main.67c25692.chunk.js"
+  },
+  {
+    "revision": "bc51d2a1e6b2c6d65a5b",
+    "url": "/static/js/runtime-main.1bd49777.js"
+  },
+  {
+    "revision": "12e53467abc753f7ffcc5a50efdf2c71",
+    "url": "/static/media/404BackGround.12e53467.png"
+  },
+  {
+    "revision": "dd317fa81f8be1d1f5f69bf25bda61bf",
+    "url": "/static/media/HeaderBackground.dd317fa8.svg"
+  },
+  {
+    "revision": "a4f0776491e8ec657acd2c068ded3788",
+    "url": "/static/media/loginBackGround.a4f07764.png"
+  },
+  {
+    "revision": "2b8ec3f79cb27a9cecd926bf65a71097",
+    "url": "/static/media/registerBackGround.2b8ec3f7.png"
+  }
+]);
